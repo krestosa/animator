a455225267829637da6db516efe505d124eccdb7
krestosa <57479846+krestosa@users.noreply.github.com>
2026-08-22 23:34:15 -0300

    Type Blink resource inventory

--- src/types/desktop.d.ts
+++ src/types/desktop.d.ts
@@ -6,6 +6,7 @@ declare global {
       blink: {
         open:(url:string)=>Promise<void>;
         close:()=>Promise<void>;
+        resources:()=>Promise<Array<{url:string;initiatorType:string;transferSize:number;decodedBodySize:number}>>;
         setViewport:(viewport:{x:number;y:number;width:number;height:number;zoomFactor:number})=>void;
         command:(command:unknown)=>void;
         onMessage:(handler:(message:unknown)=>void)=>void;
