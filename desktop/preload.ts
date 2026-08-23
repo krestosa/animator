d14218d2d1d7289d80fccb01e3befe2e522be7bc
krestosa <57479846+krestosa@users.noreply.github.com>
2026-08-22 23:45:28 -0300

    Expose Blink instrumentation toggle

--- desktop/preload.ts
+++ desktop/preload.ts
@@ -9,6 +9,7 @@ contextBridge.exposeInMainWorld('animatorDesktop',{
   blink:{
     open:(url:string)=>ipcRenderer.invoke('animator:blink:open',{url}),
     close:()=>ipcRenderer.invoke('animator:blink:close'),
+    setInstrumentation:(enabled:boolean)=>ipcRenderer.invoke('animator:blink:instrumentation',{enabled}),
     resources:()=>ipcRenderer.invoke('animator:blink:resources'),
     setViewport:(viewport:Viewport)=>ipcRenderer.send('animator:blink:viewport',viewport),
     command:(command:unknown)=>ipcRenderer.send('animator:blink:command',command),
