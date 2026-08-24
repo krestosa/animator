c9eeb2b65ba16ef4a755428fce386b25fae1dc73
krestosa <57479846+krestosa@users.noreply.github.com>
2026-08-23 23:01:28 -0300

    Complete interface refinement

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,7 @@ import './app/accessibility-refinement.css';
 import './app/product-layout.css';
 import './app/layout-groups.css';
 import './app/visual-language.css';
+import './app/inspector-refinement.css';
 import { mountApp } from './app/app';
 import { mountEditorExtras } from './app/extras';
 import { mountUiPolish } from './app/ui-polish';
